202d65b346bdbdff809c623bb630e882489cd6a0
Asilbek <asilbeksafarov962@gmail.com>
2025-05-25 10:58:57 +0500

    regionRepository connection base (and Dto for api entity)

---
+++ src/main/java/firstblock/repository/RegionRepository.java
@@ -0,0 +1,9 @@
+package firstblock.repository;
+
+import firstblock.entity.RegionEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RegionRepository extends CrudRepository<RegionEntity, Integer> {
+
+
+}
