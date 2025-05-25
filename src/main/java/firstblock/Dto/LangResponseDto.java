634fc7a59bf01a83092872cc808a164b1f8c90b4
Asilbek <asilbeksafarov962@gmail.com>
2025-05-25 20:36:09 +0500

    LangResponseDto is Sectionda AllLange responseDtoga

---
+++ src/main/java/firstblock/Dto/LangResponseDto.java
@@ -0,0 +1,13 @@
+package firstblock.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LangResponseDto {
+
+    private String name;
+    private String key;
+    private Integer id;
+}
