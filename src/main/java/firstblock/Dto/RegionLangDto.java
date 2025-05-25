7e75a723771b510b8bfd988c243bbfbfa360718a
Asilbek <asilbeksafarov962@gmail.com>
2025-05-25 11:49:54 +0500

    RegionLangDto this LangALlData Create RegionLangDto type list Api ALlData(name,id,key)

---
+++ src/main/java/firstblock/Dto/RegionLangDto.java
@@ -0,0 +1,15 @@
+package firstblock.Dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegionLangDto {
+    private Integer id;
+        private String name;
+            private String key;
+
+
+}
