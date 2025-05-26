1b05a4e249340f262aa874575515dd28b4710ecb
Asilbek <asilbeksafarov962@gmail.com>
2025-05-26 16:01:37 +0500

    Main Exceptions Working and respone Standart

---
+++ src/main/java/firstsectionblock/controller/ExceptionHandlerController.java
@@ -0,0 +1,44 @@
+package firstsectionblock.controller;
+
+import firstsectionblock.exceptionsl.AppBadException;
+import firstsectionblock.exceptionsl.NotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.*;
+
+@ControllerAdvice
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({AppBadException.class, NotFoundException.class})
+    public ResponseEntity<String> handeBadRequest(RuntimeException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        List<String> errors = new LinkedList<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getDefaultMessage());
+        }
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+}
+
