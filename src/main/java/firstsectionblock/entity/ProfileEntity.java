7a626c34b0be82534699853456e6f502dcbf45bf
Asilbek <asilbeksafarov962@gmail.com>
2025-05-26 16:05:00 +0500

    ProfileEntity connect BAse(    id,name,surname,username(email/phone),password,status,visible,created_date,photo_id. )

---
+++ src/main/java/firstsectionblock/entity/ProfileEntity.java
@@ -0,0 +1,42 @@
+package firstsectionblock.entity;
+
+
+import firstsectionblock.Enumlist.ProfileStatusEnum;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name="profile")
+public class ProfileEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private ProfileStatusEnum profileStatus=ProfileStatusEnum.ACTIVE;
+    @Column
+    private String photo_id;
+    @Column
+    private Boolean visible=true;
+    @CreationTimestamp
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdDate;
+
+
+}
